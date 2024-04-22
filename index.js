75d1eb3e2c611bf4ea43aa2009d6488a17a8b85e
githubscientist <sathish.datascientist@gmail.com>
2024-04-22 12:18:15 +0530

    connecting database

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+// import the mongoose module
+const mongoose = require('mongoose');
+
+// import the config module
+const config = require('./utils/config');
+
+console.log('Connecting to MongoDB...');
+
+// Connect to MongoDB using mongoose
+mongoose.connect(config.MongoDB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB...');
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB...', error.message);
+    })
