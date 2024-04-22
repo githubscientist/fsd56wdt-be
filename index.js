3c166e7bf76235d15f9acc6259b28591ba7a56f0
githubscientist <sathish.datascientist@gmail.com>
2024-04-22 12:27:57 +0530

    server started

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const mongoose = require('mongoose');
 // import the config module
 const config = require('./utils/config');
 
+// import the app module
+const app = require('./app');
+
 console.log('Connecting to MongoDB...');
 
 // Connect to MongoDB using mongoose
 mongoose.connect(config.MongoDB_URI)
     .then(() => {
         console.log('Connected to MongoDB...');
+        
+        // start the server
+        app.listen(config.PORT, () => {
+            console.log(`Server running on port ${config.PORT}`);
+        });
     })
     .catch((error) => {
         console.log('Error connecting to MongoDB...', error.message);
